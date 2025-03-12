31ec816618873b839d1d6f2341017f40e76a705b
joaogabiazon <jbiazonferreira@gmail.com>
2025-03-12 11:12:18 -0300

    Feat add and update user movies genres

--- src/modules/user/routes/user-router.ts
+++ src/modules/user/routes/user-router.ts
@@ -4,6 +4,7 @@ import CreatUserController from "../controllers/create-controller";
 import GetUserController from "../controllers/get-controller";
 import UpdateUserController from "../controllers/update-controller";
 import DeleteUserController from "../controllers/delete-controller";
+import AddGenresController from "../controllers/add_genres-controller";
 const router = Router()
 
 // rota de registro de usuário
@@ -11,4 +12,5 @@ router.post('/createuser', CreatUserController.createUser);
 router.get('/getuser/:uid', GetUserController.getUser);
 router.put('/updateuser', UpdateUserController.updateUser);
 router.put('/deleteuser', DeleteUserController.deleteUser);
+router.put('/addgenres', AddGenresController.handleGenres);
 export default router;

94a84df2fb29004da66e53db885a9b96e672c3c7
joaogabiazon <jbiazonferreira@gmail.com>
2025-03-20 09:35:14 -0300

    Fix filter to get more accertive filters

--- src/modules/movies/services/get_movies-service.ts
+++ src/modules/movies/services/get_movies-service.ts
@@ -5,7 +5,25 @@ class GetMovies {
     async getMovies(gentres: any): Promise<Document[] | null> {
         const genres = gentres;
         try {
-            const movies = await Movie.find({ genres: { $in: genres } });
+
+            const movies = await Movie
+            .find(
+                { 
+                    genres: { $in: genres },
+                    year: { $gte: 2000 },
+                    "imdb.rating": { $gte: 5 },
+                    "imdb.votes": { $gte: 300 },
+                    type: "movie",
+                    "awards.wins": { $gte: 1 },
+                    "tomatoes.viewer.rating": { $gte: 4 },
+                    runtime: { $gte: 90 },
+                    poster: { $exists: true, $ne: null }
+                }
+            )
+            .select("_id fullplot title poster genres")
+            .sort({"imdb.rating": -1})
+            .limit(20);
+
             return movies;
         } catch (error) {
             console.error(error);

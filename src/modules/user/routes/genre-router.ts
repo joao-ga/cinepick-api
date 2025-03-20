9db008624bad1b867d2890bed5545fdec541516c
Caio Acosta <caioacosta4@gmail.com>
2025-03-20 08:44:01 -0300

    get_all_genres correction

--- src/modules/user/routes/genre-router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-const genreController = require("../controllers/genreController");
-
-router.get("/getallgenres", genreController.getGenres);
-
-export default router;

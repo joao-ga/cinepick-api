301b98a7954dd330ceaaca4a4033e23e08dedfc4
Enzo Quatrochi <enzoquatrochi@outlook.com>
2025-03-23 20:52:11 -0300

    small fixes

--- src/modules/movies/router/movie-router.ts
+++ src/modules/movies/router/movie-router.ts
@@ -3,7 +3,6 @@ import GetMoviesController from "../controllers/get_movies-controller";
 import GetPosterController from "../controllers/get_poster-controller";
 
 const router = Router()
-console.log("Configuração do roteador '/movies' iniciada.");
 
 router.get('/getmovies', GetMoviesController.getMovies);
 router.get('/getposter', GetPosterController.getPoster);

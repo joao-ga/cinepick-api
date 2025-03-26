e53200664290a37b84102f47ca24088cd39531d2
Caio Acosta <caioacosta4@gmail.com>
2025-03-26 10:48:30 -0300

    Adding 'movies' field in user database and addMovies method

--- src/modules/user/routes/user-router.ts
+++ src/modules/user/routes/user-router.ts
@@ -4,6 +4,7 @@ import GetUserController from "../controllers/get-controller";
 import UpdateUserController from "../controllers/update-controller";
 import DeleteUserController from "../controllers/delete-controller";
 import AddGenresController from "../controllers/add_genres-controller";
+import AddMoviesController from "../controllers/add_movies-controller";
 const router = Router()
 
 // rota de registro de usuário
@@ -12,4 +13,5 @@ router.get('/getuser/:uid', GetUserController.getUser);
 router.put('/updateuser', UpdateUserController.updateUser);
 router.put('/deleteuser', DeleteUserController.deleteUser);
 router.put('/addgenres', AddGenresController.handleGenres);
+router.put('/addmovies', AddMoviesController.handleMovies);
 export default router;

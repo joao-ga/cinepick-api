f15b41915899cbf63f36acf9c54c1960f9e40a33
Enzo <enzoquatrochi@outlook.com>
2025-11-04 21:19:23 -0300

    feat/create user follow each other verification route

--- src/modules/follows/routes/follow-router.ts
+++ src/modules/follows/routes/follow-router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import FollowUserController from "../controllers/follow-user-controller";
 import UnfollowUserController from "../controllers/unfollow-user-controller";
+import FollowEachOtherController from "../controllers/follow_each_other-controller";
 
 const router = Router();
 
 // Rotas de seguir usuários
 router.post("/follow", FollowUserController.follow);
 router.post("/unfollow", (req, res, next) => { void UnfollowUserController.unfollow(req, res).catch(next); });
+router.get("/follow-each-other", (req, res, next) => { void FollowEachOtherController.followEachOther(req, res).catch(next); });
 
 export default router;
